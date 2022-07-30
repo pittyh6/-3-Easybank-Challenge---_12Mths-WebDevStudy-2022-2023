83037d884814e0ecb02f7791028c920071e1ff12
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-07-30 12:52:19 +1000

    [wds-302] start progress

---
+++ easybank-landing-page-master/scripts/toggle.js
@@ -0,0 +1,11 @@
+
+$(function() {
+    $(".toggle").on("click", function(){
+        if($(".nav-item").hasClass("active")){
+            $(".nav-item").removeClass("active");
+        }else{
+            $(".nav-item").addClass("active");
+        }
+    })
+})
+console.log($(".toggle"))
