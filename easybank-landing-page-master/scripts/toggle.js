0dfa95fca39719992324e711d94018a83f8b8493
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-07-30 17:16:25 +1000

    [wds-302][wds-303] navbar mobile and desktop

--- easybank-landing-page-master/scripts/toggle.js
+++ easybank-landing-page-master/scripts/toggle.js
@@ -3,9 +3,10 @@ $(function() {
     $(".toggle").on("click", function(){
         if($(".nav-item").hasClass("active")){
             $(".nav-item").removeClass("active");
+            $("#toggle-img").attr("src", "./images/icon-hamburger.svg");
         }else{
             $(".nav-item").addClass("active");
+            $("#toggle-img").attr("src", "./images/icon-close.svg");
         }
     })
 })
-console.log($(".toggle"))
